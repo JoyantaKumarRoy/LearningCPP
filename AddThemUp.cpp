d52d0162baeb893d25c596d65c990617fc522482
Joyanta Kumar <joyroy.primary@gmail.com>
2024-11-20 17:17:04 +0600

    Function overloaded.

--- AddThemUp.cpp
+++ AddThemUp.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
 int add(int, int);
+double add(double, double);
 
 int main() {
 
-    int a, b;
+    double a, b;
     std::cout << "Two number: ";
     std::cin >> a >> b;
     std::cout << add(a, b);
 }
 
 int add(int x, int y) {
+    return x + y;
+}
+double add(double x, double y) {
     return x + y;
 }
